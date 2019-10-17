53212474e0dad332d299e6e172848cc470eb5d67
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-10-17 16:57:21 -0500

    set up index including server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const {SERVER_PORT} = process.env
+
+const app = express()
+app.use(express.json());
+
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance);
+    console.log("database connected :D")
+})
+
+app.use(session ({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 3
+    }
+}))
+
+app.listen(SERVER_PORT, () => console.log(`listening on ${SERVER_PORT}`));
