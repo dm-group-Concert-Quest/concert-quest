ea2a01b9446340d015556a8721044ca0362fa994
Noel N. Contreras <noeln.contreras@Noels-MacBook-Air.local>
2019-10-18 17:07:37 -0500

    working on nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import {withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import { getSession, logoutUser } from "../redux/reducers/userReducer";
+import "../styles/_Nav.scss";
 
-export default class Nav extends Component {
+class Nav extends Component {
+
+    // componentDidMount() {
+    //     this.props.getSession();
+    // }
+
     render() {
+        const { userId } = this.props;
         return (
-            <div>
-                <div>
-                    <h1>Concert Quest</h1>
-                </div>
-                <div>
-                    <h2>About</h2>
-                    <h2>Register</h2>
-                    <h2>Contact</h2>
-                </div>
-            </div>
+            <nav className="Nav-nav-container">
+                {!userId ?
+                    <>
+                        <div className="Nav-nav-title-logout">
+                            <h1>Concert Quest</h1>
+                        </div>
+                        <div className="Nav-nav-links-logout">
+                            <h2>Register</h2>
+                            <h2>About</h2>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div className="Nav-nav-container">
+                            <div className="Nav-nav-title-login">
+                                <h1>Concert Quest</h1>
+                            </div>
+                        </div>
+                        <div className="Nav-nav-links-login">
+                            <h2>Home</h2>
+                            <h2>Profile</h2>
+                            <h2>About</h2>
+                            <h2>Settings</h2>
+                            <button>LOGOUT</button>
+                        </div>
+                    </>
+                }
+            </nav>
         )
     }
 }
+
+const mapStateToProps = reduxState => {
+    return {
+        userId: reduxState.userReducer.userId
+    }
+}
+
+export default withRouter(connect(mapStateToProps, { getSession, logoutUser })(Nav));
