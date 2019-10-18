9254ec2dc637684e62c245561a611693954c8791
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-10-18 11:53:45 -0500

    create userLanding About and TrackedBands components

---
+++ src/components/TrackedBands.js
@@ -0,0 +1,18 @@
+import React from "React"
+
+
+class TrackedBands extends React.Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <h1>Tracked Bands!</h1>
+        )
+    }
+}
+
+export default TrackedBands;
