3229b33939b9156798e169e0e5e7d491545e9a66
Trey Waller <Trey@Treys-MacBook-Pro.local>
2019-10-22 17:21:00 -0500

    fixing stuuupid merge conflicts

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import {getSession} from '../redux/reducers/userReducer';
+import {connect} from 'react-redux';
 
-export default class UserProfile extends Component {
+class UserProfile extends Component {
+
     render() {
         return (
             <div id='profile-bg'>
                 <div id='user-profile'>
-                    
+
                 </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = reduxState => {
+    return {
+        first_name: reduxState.userReducer.first_name
+    }
+}
+
+export default connect(mapStateToProps, {getSession})(UserProfile);
