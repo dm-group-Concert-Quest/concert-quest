aaad700b15ee8e364a8375e36e3255fd4f1b5a2a
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-10-22 15:23:36 -0500

    deleted noel and trey folder as they are going to make them themselves, also added tests to my testing folder

--- src/__test__/mads.test.js
+++ src/__test__/mads.test.js
@@ -1,1 +1,20 @@
-swag moeny 
+import {logoutUser, updateUsername, updatePassword, updateEmail} from "../redux/reducers/userReducer";
+
+
+
+
+
+
+
+test('Test to see type of logoutUser == "LOGOUT_USER" ', () => {
+   expect(logoutUser().type).toBe("LOGOUT_USER")
+})
+test('Test to see type of updateUsername == "UPDATE_USERNAME" ', () => {
+   expect(updateUsername().type).toBe("UPDATE_USERNAME")
+})
+test('Test to see type of updatePassword == "UPDATE_PASSWORD" ', () => {
+   expect(updatePassword().type).toBe("UPDATE_PASSWORD")
+})
+test('Test to see type of updateEmail == "UPDATE_EMAIL" ', () => {
+   expect(updateEmail().type).toBe("UPDATE_EMAIL")
+})
