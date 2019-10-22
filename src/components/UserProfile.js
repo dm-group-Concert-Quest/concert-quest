92e99e1b793233fb2d2cf1c4cb1eb80d6c9a1580
Trey Waller <Trey@Treys-MacBook-Pro.local>
2019-10-22 16:57:49 -0500

    starting user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class UserProfile extends Component {
     render() {
         return (
-            <div>
-                Profile
+            <div id='profile-bg'>
+                <div id='user-profile'>
+                    
+                </div>
             </div>
         )
     }
