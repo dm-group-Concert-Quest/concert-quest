560851c759b9a3b07132a7ad6058a5aa8bc05333
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-10-22 16:57:13 -0500

    finished unit testing and some other stuff

--- src/__test__/mads.test.js
+++ src/__test__/mads.test.js
@@ -1,11 +1,5 @@
 import {logoutUser, updateUsername, updatePassword, updateEmail} from "../redux/reducers/userReducer";
 
-
-
-
-
-
-
 test('Test to see type of logoutUser == "LOGOUT_USER" ', () => {
    expect(logoutUser().type).toBe("LOGOUT_USER")
 })
@@ -17,4 +11,7 @@ test('Test to see type of updatePassword == "UPDATE_PASSWORD" ', () => {
 })
 test('Test to see type of updateEmail == "UPDATE_EMAIL" ', () => {
    expect(updateEmail().type).toBe("UPDATE_EMAIL")
+})
+test('Test to see type of updateUsername == {} ', () => {
+   expect(updateUsername().payload).resolves.toEqual({})
 })
