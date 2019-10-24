43a94c2a178be2148fa494181c8567b213ab2078
Noel N. Contreras <noeln.contreras@Noels-MacBook-Air.local>
2019-10-23 21:28:54 -0500

    worked on guestlanding

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -26,24 +26,39 @@ class EventList extends Component {
                 `https://rest.bandsintown.com/artists/post%20malone?app_id=${REACT_APP_BAND_APP_KEY}`
             )
             .then(response => {
+                console.log(response.data)
                 this.setState({ artist: response.data });
             });
     }
 
     render() {
         const { artist } = this.state;
-
         const eventsMapped = this.state.events.map((event, i) => {
+            const str = event.offers[0].status;
+            const status = str.toLowerCase()
+                .split(' ')
+                .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                .join(' ');
+            const date = new Date(event.datetime);
+            var options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
             return (
                 <div key={i} className='event-container'>
-                    <img className='event-image' src={artist.image_url} alt="artist-pic" />
-                    <h4>{event.venue.name}</h4>
-                    <h6>{event.venue.city}</h6>
+                    <div className="event-header">
+                        <img className='event-image' src={artist.image_url} alt="artist-pic" />
+                        {event.lineup.length > 1 ? <p className="event-lineup">{`${event.lineup[0]}, ${event.lineup[1]}, ${event.lineup[2]}...`}</p> : <p className="event-lineup">{event.lineup}</p>}
+                    </div>
+                    <div className="event-venue-info">
+                        <h4>{event.venue.name}</h4>
+                        <h4>{`${event.venue.city}, ${event.venue.region}`}</h4>
+                        <h4>{date.toLocaleDateString("en-US", options)}</h4>
+                        <br />
+                    </div>
                     {!this.props.user_id ?
                         null
                         :
-                        <div>
-                            <a target="_blank" rel="noopener noreferrer" href={event.offers[0].url}><button>Tickets!!!</button></a>
+                        <div className="event-ticket-box">
+                            <a target="_blank" rel="noopener noreferrer" href={event.offers[0].url}><button className="event-ticket-button">Tickets</button></a>
+                            <p>{status}</p>
                         </div>
                     }
                 </div>
@@ -51,6 +66,9 @@ class EventList extends Component {
         });
         return (
             <div>
+                <div>
+                    <h1>Login to buy tickets</h1>
+                </div>
                 <div className='events-list'>
                     {eventsMapped}
                 </div>
